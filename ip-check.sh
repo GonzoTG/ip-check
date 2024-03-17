286a16e024ad872855f6b818fc38a6af300d55a2
Greg Lowe <18699503+GonzoTG@users.noreply.github.com>
2024-03-17 07:14:20 -0400

    Initial Commit

---
+++ ip-check.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# check and send ip address to email
+MYIP=`curl --ipv4 ifconfig.me`;
+TIME=`date`;
+LASTIPFILE='/opt/ip-check/.last_ip_addr';
+touch ${LASTIPFILE}
+LASTIP=`cat ${LASTIPFILE}`;
+
+if [[ ${MYIP} != ${LASTIP} ]]
+then
+  pushover-cli push "Public IP changed for '$(hostname -d)'. Old IP: ${LASTIP}, New IP: ${MYIP}";
+  echo ${MYIP} > ${LASTIPFILE};
+else
+        echo "no IP change!"
+fi
